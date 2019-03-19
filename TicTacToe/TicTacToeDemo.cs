cf1d48f6da82c33a5f769cb38d099364b9ee8331
Tyreak Allen <allentyreak@gmail.com>
2019-03-19 18:02:31 -0400

    first commit

---
+++ TicTacToe/TicTacToeDemo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class TicTacToeDemo
+    {
+        string CurrentPlayer;
+
+        public int CreateBoard (string[,] array)
+        {
+            return array.Length;
+        }
+
+        public string currentPlayer (string player1)
+        {
+            //tracks the current player
+            return CurrentPlayer = player1;
+
+        }
+
+        public string changePlayer(string player1, string player2)
+        {
+            //changes the player after the current player made a move
+            player1 = player2;
+            CurrentPlayer = player1;
+            return CurrentPlayer;
+        }
+
+        public string makeMove(string [,] array, string player1)
+        {
+            //current player puts only a X or an O in a empty space
+            array[1, 2] = player1;
+            return array[1, 2];
+        }
+
+        public bool spaceInUse(string[,] array)
+        {
+            //tracks which spaces in the array already have a X or a O
+            if (array[1,2] == " ")
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ TicTacToeTests/TicTacToeDemoTests.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Tests
+{
+    [TestClass()]
+    public class TicTacToeDemoTests
+    {
+        [TestMethod()]
+        public void CreateBoardTest()
+        {
+            TicTacToeDemo game = new TicTacToeDemo();
+            string[,] newGame = new string[3,3];
+            game.CreateBoard(newGame);
+            Assert.AreEqual(9, newGame.Length);
+        }
+
+        [TestMethod()]
+        public void currentPlayerTest()
+        {
+            TicTacToeDemo game = new TicTacToeDemo();
+            string[,] newGame = new string[3, 3];
+            string playerX = "X";
+            //game.currentPlayer(playerX);
+            Assert.AreEqual("X", game.currentPlayer(playerX));
+        }
+
+        [TestMethod()]
+        public void makeMoveTest()
+        {
+            TicTacToeDemo game = new TicTacToeDemo();
+            string[,] newGame = new string[3, 3];
+            string playerX = "X";
+            //game.makeMove(newGame, playerX);
+            Assert.AreEqual("X", game.makeMove(newGame, playerX));
+        }
+
+        [TestMethod()]
+        public void spaceTest()
+        {
+            TicTacToeDemo game = new TicTacToeDemo();
+            string[,] newGame = new string[3, 3];
+            string playerX = "X";
+            game.makeMove(newGame, playerX);
+            //game.spaceInUse(newGame);
+            Assert.AreEqual(true, game.spaceInUse(newGame));
+        }
+
+        [TestMethod()]
+        public void changePlayerTest()
+        {
+            TicTacToeDemo game = new TicTacToeDemo();
+            string[,] newGame = new string[3, 3];
+            string playerX = "X";
+            string playerO = "O";
+            //game.changePlayer(playerX, playerO);
+            Assert.AreEqual("O", game.changePlayer(playerX, playerO));
+        }
+
+        [TestMethod()]
+        public void currentPlayerOTest()
+        {
+            TicTacToeDemo game = new TicTacToeDemo();
+            string[,] newGame = new string[3, 3];
+            string playerO = "O";
+            //game.currentPlayer(playerO);
+            Assert.AreEqual("O", game.currentPlayer(playerO));
+        }
+
+        [TestMethod()]
+        public void makeMoveOTest()
+        {
+            TicTacToeDemo game = new TicTacToeDemo();
+            string[,] newGame = new string[3, 3];
+            string playerO = "O";
+            //game.makeMove(newGame, playerO);
+            Assert.AreEqual("O", game.makeMove(newGame, playerO));
+
+        }
+
+        [TestMethod()]
+        public void spaceOTest()
+        {
+            TicTacToeDemo game = new TicTacToeDemo();
+            string[,] newGame = new string[3, 3];
+            string playerO = "O";
+            game.makeMove(newGame, playerO);
+            //game.spaceInUse(newGame);
+            Assert.AreEqual(true, game.spaceInUse(newGame));
+
+        }
+
+    }
+}
